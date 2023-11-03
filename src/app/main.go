9d03bb064df6fecaa3da1914f77dae899b9f1e97
Jason Cheng <jasoncky96@gmail.com>
2023-11-03 23:41:35 +0800

    made production to build template files as well

--- src/app/main.go
+++ src/app/main.go
@@ -37,17 +37,4 @@ func main() {
 	e.GET("/", handlers.HomePage)
 
 	e.Logger.Fatal(e.Start(":8080"))
-	// router := gin.Default()
-	// router.LoadHTMLGlob("templates/*")
-
-	// router.GET("/", handlers.HomePage)
-	// router.Static("/static", "./static")
-	// s := &http.Server{
-	// 	Addr:           ":8080",
-	// 	Handler:        router,
-	// 	ReadTimeout:    10 * time.Second,
-	// 	WriteTimeout:   10 * time.Second,
-	// 	MaxHeaderBytes: 1 << 20,
-	// }
-	// s.ListenAndServe()
 }
