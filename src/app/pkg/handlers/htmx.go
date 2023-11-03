afbd6258543f7e38954c04c0215e3b4e1d232cab
Jason Cheng <jasoncky96@gmail.com>
2023-11-04 00:37:55 +0800

    make local dev container have all needed deps to live reload the tailwind css, golang templ, and go binary

--- src/app/pkg/handlers/htmx.go
+++ src/app/pkg/handlers/htmx.go
@@ -3,11 +3,11 @@ package handlers
 import (
 	"context"
 
-	"github.com/Jason-CKY/htmx-todo-app/pkg/views"
+	"github.com/Jason-CKY/htmx-todo-app/pkg/components"
 	"github.com/labstack/echo/v4"
 )
 
 func HomePage(c echo.Context) error {
-	component := views.HomePage(5)
+	component := components.HomePage(5)
 	return component.Render(context.Background(), c.Response().Writer)
 }
